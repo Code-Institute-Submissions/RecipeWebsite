4b0a5b310cac5159c706c78a37566815041cdb62
Antonio Cesarino <ant.ces86@outlook.com>
2024-06-29 23:50:56 +0000

    Fix issue with local host

--- main/settings.py
+++ main/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '8000-t0tacci0-p4-qs1ajprhgsy.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '8000-t0tacci0-recipewebsite-c95v7alg7wr.ws.codeinstitute-ide.net']
 
 
 # Application definition
@@ -195,5 +195,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = "staticfiles"
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://8000-t0tacci0-p4-qs1ajprhgsy.ws.codeinstitute-ide.net/'
+    'https://8000-t0tacci0-recipewebsite-c95v7alg7wr.ws.codeinstitute-ide.net'
 ]
