863737a6bb2bef4a56121638b63332bc217e3c57
Antonio Cesarino <ant.ces86@outlook.com>
2024-07-01 03:03:21 +0000

    Update database

--- main/settings.py
+++ main/settings.py
@@ -13,6 +13,8 @@ import os
 from pathlib import Path
 import django_heroku
 import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 import cloudinary
 
 DATABASES = {
@@ -135,10 +137,7 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
